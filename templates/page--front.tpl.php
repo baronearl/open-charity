0bfe762c6e783f4fd5b1b49fe0f926c741acf04a
Baronearl <Baronearl>
2017-01-16 22:47:09 +0100

    First commit

---
+++ templates/page--front.tpl.php
@@ -0,0 +1,139 @@
+<?php //include_once($theme_path.'/include/header.tpl.php');  // include $directory . '/charity/include/headers.php';         ?>
+
+<div id="page_wrapper">
+    <div id="middle-center">
+        <h1>SHARING IDEAS FOR CHARITIES</h1>
+        <div id="home_description">
+            <p>
+                Many charities's goals are similar, as is the functionality we require, but little shared working takes
+                place.
+            </p>
+
+            <p>
+                By working together, driving shared areas of interest and influencing open source developments we can
+                bring efficiencies,
+                improve the digital experience for our users, and have great impact.
+            </p>
+            <p>
+                Together we can make a bigger difference.
+            </p>
+        </div>
+    </div>
+    <div id="next_crumb">
+        <div id="next_info">
+            <div id="next_event"><span>Next Event: </span> <span>June 23<sup>rd</sup> 2016 18:30 - 21:30</span></div>        
+            <div id="next_desc">Cancer Research UK, Angel Building, 407 St John Street, London EC1V 4AD</div>
+        </div>
+        <div id="register_span"><a class="style_register" href="register">Register</a></div>
+    </div>
+    <div id="low-int-la">
+        <h2>GET INVOLVED</h2>
+        <div class="mid-clear">
+            <div class="mid-img"><img src="<?php print base_path() . path_to_theme(); ?>/images/meet.png"/></div>
+            <div class="mid-clear-head">WE DO MEETINGS</div>
+            <div class="mid-clear-description">We organize meeting through the OpenCharity MeetUp group.</div>
+            <div class="mid-action-intake"><a class="style_up" href="<?php echo $GLOBALS['base_url'] . '/group'; ?>">MEETUP GROUP</a> </div>
+        </div>
+        <div class="mid-clear">
+            <div class="mid-img"><img src="<?php print base_path() . path_to_theme(); ?>/images/hash.png"/></div>
+            <div class="mid-clear-head">WE COLLABORATE</div>
+            <div class="mid-clear-description">Open Charity have a slack group for daily collaboration </div>
+            <div class="mid-action-intake"><a class="style_up" href="<?php echo $GLOBALS['base_url'] . '/slack'; ?>">SLACK GROUP</a></div>
+        </div>
+        <div class="mid-clear">
+            <div class="mid-img"><img src="<?php print base_path() . path_to_theme(); ?>/images/group.png"/></div>
+            <div class="mid-clear-head">WE SHARE</div>
+            <div class="mid-clear-description">We have a Google Group set up to share tools and documents</div>
+            <div class="mid-action-intake"><a class="style_up" href="<?php echo $GLOBALS['base_url'] . '/google-group'; ?>">GOOGLE GROUP</a></div>
+        </div>
+    </div>
+    <div id="low-aft-la">
+        <div id="our-mission">
+            <h2>OUR MISSION</h2>
+            <div class="inter-div">
+                <div>Charities and Partners collaborating and sharing open solutions and ideas to create value in the digital space.</div>
+                <strong>If you are a charity or a supplier, we are ready to welcome you.</strong>
+            </div>
+            <div id="encap-with-bord">
+                <div id="ind-mission">
+                    <div class="mem-disp">
+                        <div class="mem-disp-img"><img src="<?php print base_path() . path_to_theme(); ?>/images/bulb.png" alt="We help charities"/></div>
+                        <div class="mem-disp-title">We help charities</div>
+                        <div class="mem-disp-desc">share knowledge and working practice to make the best technology choices.</div>
+                    </div>
+                    <div class="mem-disp">
+                        <div class="mem-disp-img"><img src="<?php print base_path() . path_to_theme(); ?>/images/friends.png" alt="We bring together"/></div>
+                        <div class="mem-disp-title">We bring together</div>
+                        <div class="mem-disp-desc">charities and suppliers to the charity sector to share best practices.</div>
+                    </div>
+                    <div class="mem-disp">
+                        <div class="mem-disp-img"><img src="<?php print base_path() . path_to_theme(); ?>/images/thumb.png" alt="We encourage"/></div>
+                        <div class="mem-disp-title">We encourage </div>
+                        <div class="mem-disp-desc">collaboration and innovation for the good of the charity sector.</div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <h2>OUR MEMBERS</h2>
+        <div id="all-members">
+            <div class="each"><img src="<?php print base_path() . path_to_theme(); ?>/images/members.png"/></div>
+            <div class="e_spots"><span></span><span></span><span></span></div>
+        </div>
+    </div>
+    <div id="low-after-blog">
+        <h2>BLOG</h2>
+        <div id="horiz-blog">
+            <div id="left_blog_nav"><span></span></div>
+            <div class="blog-disp">
+                <h3>Online Donations Special...</h3> 
+                <div class="blog-desc">
+                    Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit, sed do 
+                    elusmod tempor incididunt ut labore et
+                    dolore magna aliqua.
+                </div>
+
+                <span class="blog-date">14 Nov 2014</span>
+            </div>
+
+            <div class="blog-disp">
+                <h3>Online Donations Special...</h3> 
+                <div class="blog-desc">
+                    Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit, sed do 
+                    elusmod tempor incididunt ut labore et
+                    dolore magna aliqua.
+                </div>
+
+                <span class="blog-date">14 Nov 2014</span>
+            </div>
+
+            <div class="blog-disp">
+                <h3>Online Donations Special...</h3> 
+                <div class="blog-desc">
+                    Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit, sed do 
+                    elusmod tempor incididunt ut labore et
+                    dolore magna aliqua.
+                </div>
+
+                <span class="blog-date">14 Nov 2014</span>
+            </div>
+            
+            <div class="blog-disp">
+                <h3>Online Donations Special...</h3> 
+                <div class="blog-desc">
+                    Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit, sed do 
+                    elusmod tempor incididunt ut labore et
+                    dolore magna aliqua.
+                </div>
+
+                <span class="blog-date">14 Nov 2014</span>
+            </div>
+            <div id="right_blog_nav"><span></span></div>
+        </div>
+    </div>
+
+</div>
